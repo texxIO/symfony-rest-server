0a4e50292636c6a7e0c0f546be018ec04b356e7d
Ionut Mihaescu <ionut.mihaescu@telekom.ro>
2018-06-13 16:18:43 +0300

    more try catch and code clean

--- src/AppBundle/Controller/Api/OffersController.php
+++ src/AppBundle/Controller/Api/OffersController.php
@@ -1,137 +1,133 @@
 <?php
-namespace AppBundle\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Repository\OfferRepository;
-use AppBundle\Entity\Offer;
+    namespace AppBundle\Controller\Api;
 
-class OffersController extends Controller
-{
-    /**
-     * @param Request $request
-     * @Route("/api/offers", methods={"GET"})
-     * @return Response
-     */
-    public function getAllAction(Request $request)
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use AppBundle\Entity\Offer;
+
+    class OffersController extends Controller
     {
-        $repository = $this->getDoctrine()->getRepository(Offer::class);
-        $offers = $repository->findAll();
-        return $this->json($offers, 200);
-    }
-
-    /**
-     * @param int $offerId
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     * @Route("/api/offers/{offerId}", methods={"GET"})
-     */
-    public function getByIdAction( int $offerId, Request $request)
-    {
-        if (  $offerId > 0 )
+        /**
+         * @return Response
+         * @Route("/api/offers", methods={"GET"})
+         */
+        public function getAllAction()
         {
-            $repository = $this->getDoctrine()->getRepository(Offer::class);
-            $offer = $repository->find($offerId);
-            return $this->json($offer, 200);
-        }
-        return $this->json(['status'=>'Fetch error , check API server log'], 404);
-    }
+            try {
+                $repository = $this->getDoctrine()->getRepository(Offer::class);
+                $offers = $repository->findAll();
+                return $this->json($offers, 200);
+            } catch (\Exception $e) {
+                return $this->json(['status' => 'Fetch error , check API server log'], 404);
+            }
 
-    /**
-     * @param Request $request
-     * @Route("/api/offer", methods={"POST"})
-     * @return string
-     */
-    public function addAction( Request $request )
-    {
-        /*
-        $offerData['title'] = $request->request->get('title');
-        $offerData['description'] = $request->request->get('description');
-        $offerData['email'] = $request->request->get('email');
-        $offerData['image_url'] = $request->request->get('image_url');
-        $offerData['creation_date'] = new \DateTime("now");
-        */
-
-        try
-        {
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $offer = new Offer();
-            $offer->setTitle($request->request->get('title'));
-            $offer->setDescription($request->request->get('description'));
-            $offer->setEmail($request->request->get('email'));
-            $offer->setImageUrl($request->request->get('image_url'));
-            $offer->setCreationDate(new \DateTime("now"));
-
-            $entityManager->persist($offer);
-            $entityManager->flush();
-
-            return $this->json(['offerId'=>$offer->getId()], 200);
-        }
-        catch ( \Exception $e )
-        {
-            return $this->json(['status'=>'Add error , check API server log'], 404);
         }
 
+        /**
+         * @param int $offerId
+         * @param Request $request
+         * @return \Symfony\Component\HttpFoundation\JsonResponse
+         * @Route("/api/offers/{offerId}", methods={"GET"})
+         */
+        public function getByIdAction(int $offerId)
+        {
+            try {
+                $repository = $this->getDoctrine()->getRepository(Offer::class);
+                $offer = $repository->find($offerId);
+                return $this->json($offer, 200);
+            } catch (\Exception $e) {
+                return $this->json(['status' => 'Fetch error , check API server log'], 404);
+            }
 
-    }
+        }
 
-    /**
-     * @param Request $request
-     * @Route("/api/offers/delete/{offerId}",requirements={"offerId"="\d+"},  methods={"GET"})
-     * @return string
-     */
-    public function deleteAction( int $offerId )
-    {
+        /**
+         * @param Request $request
+         * @Route("/api/offer", methods={"POST"})
+         * @return string
+         */
+        public function addAction(Request $request)
+        {
 
-        if ( isset($offerId) && is_int($offerId) && intval($offerId) > 0 ) {
             try {
                 $entityManager = $this->getDoctrine()->getManager();
-                $offer = $entityManager->getRepository(Offer::class)->find($offerId);
-                $entityManager->remove($offer);
+
+                $offer = new Offer();
+                $offer->setTitle($request->request->get('title'));
+                $offer->setDescription($request->request->get('description'));
+                $offer->setEmail($request->request->get('email'));
+                $offer->setImageUrl($request->request->get('image_url'));
+                $offer->setCreationDate(new \DateTime("now"));
+
+                $entityManager->persist($offer);
                 $entityManager->flush();
-                return $this->json([$offer], 200);
-            }
-            catch ( \Exception $e )
-            {
-                return $this->json(['status'=>'Delete error , check API server log'], 404);
+
+                return $this->json(['offerId' => $offer->getId()], 200);
+            } catch (\Exception $e) {
+                return $this->json(['status' => 'Add error , check API server log'], 404);
             }
 
 
         }
 
-        return $this->json(['status'=>'The offer ID is missing or is not valid'], 404);
-    }
-
-    /**
-     * @param Request $request
-     * @Route("api/update/{offerId}", methods={"PUT", "PATCH", "GET"})
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function updateAction( int $offerId, Request $request )
-    {
-        if ( !is_int($offerId) )
+        /**
+         * @param int $offerId
+         * @return string
+         * @Route("/api/offers/delete/{offerId}",requirements={"offerId"="\d+"},  methods={"GET"})
+         */
+        public function deleteAction(int $offerId)
         {
-            return $this->json(['status'=>'Fetch error , check API server log'], 404);
+
+            if (isset($offerId) && is_int($offerId) && intval($offerId) > 0) {
+                try {
+                    $entityManager = $this->getDoctrine()->getManager();
+                    $offer = $entityManager->getRepository(Offer::class)->find($offerId);
+                    $entityManager->remove($offer);
+                    $entityManager->flush();
+                    return $this->json([$offer], 200);
+                } catch (\Exception $e) {
+                    return $this->json(['status' => 'Delete error , check API server log'], 404);
+                }
+
+
+            }
+
+            return $this->json(['status' => 'The offer ID is missing or is not valid'], 404);
         }
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $offer = $this->getDoctrine()
-            ->getRepository(Offer::class)
-            ->find($offerId);
+        /**
+         * @param Request $request
+         * @Route("api/update/{offerId}", methods={"PUT", "PATCH", "GET"})
+         * @return \Symfony\Component\HttpFoundation\JsonResponse
+         */
+        public function updateAction(int $offerId, Request $request)
+        {
+            if (!is_int($offerId)) {
+                return $this->json(['status' => 'Fetch error , check API server log'], 404);
+            }
 
-        $offer->setTitle($request->request->get('title'));
-        $offer->setDescription($request->request->get('description'));
-        $offer->setEmail($request->request->get('email'));
-        $offer->setImageUrl($request->request->get('image_url'));
+            try {
+                $entityManager = $this->getDoctrine()->getManager();
+                $offer = $this->getDoctrine()
+                    ->getRepository(Offer::class)
+                    ->find($offerId);
 
-        $entityManager->flush();
+                $offer->setTitle($request->request->get('title'));
+                $offer->setDescription($request->request->get('description'));
+                $offer->setEmail($request->request->get('email'));
+                $offer->setImageUrl($request->request->get('image_url'));
 
-        return $this->json(['status'=>'Offer Updated'], 200);
+                $entityManager->flush();
+
+                return $this->json(['status' => 'Offer updated'], 200);
+
+            } catch (\Exception $e) {
+                return $this->json(['status' => 'Offer update failed'], 404);
+            }
+
+        }
 
     }
-
-
-}
